cf86a775d3cfb008444d888c016af9bbae038117
safarikales305 <safarikales305@gmail.com>
2026-04-04 22:49:25 +0200

    force redeploy health test

--- nestjs-backend/src/health.controller.ts
+++ nestjs-backend/src/health.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class HealthController {
   @Get()
   getHealth() {
-    return { status: 'ok v2' };
+    return { status: 'ok FINAL123' };
   }
 }
