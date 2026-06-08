c5cd4ec13c642b562bcf26baacae1222bf205ae5
safarikales305 <safarikales305@gmail.com>
2026-06-08 21:02:18 +0200

    Fix bonus campaign DTO build errors

--- nestjs-backend/src/modules/bonus-campaign/dto/update-bonus-campaign.dto.ts
+++ nestjs-backend/src/modules/bonus-campaign/dto/update-bonus-campaign.dto.ts
@@ -1,4 +1,44 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBonusCampaignDto } from './create-bonus-campaign.dto';
+import { BonusAppliesTo } from '@prisma/client';
+import { IsBoolean, IsEnum, IsInt, IsOptional, IsString, MaxLength, Min } from 'class-validator';
 
-export class UpdateBonusCampaignDto extends PartialType(CreateBonusCampaignDto) {}
+export class UpdateBonusCampaignDto {
+  @IsOptional()
+  @IsString()
+  @MaxLength(200)
+  title?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(200)
+  ctaText?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(300)
+  bonusText?: string;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  amount?: number;
+
+  @IsOptional()
+  @IsEnum(BonusAppliesTo)
+  appliesTo?: BonusAppliesTo;
+
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean;
+
+  @IsOptional()
+  @IsString()
+  activeFrom?: string | null;
+
+  @IsOptional()
+  @IsString()
+  activeTo?: string | null;
+
+  @IsOptional()
+  @IsBoolean()
+  oncePerUser?: boolean;
+}
